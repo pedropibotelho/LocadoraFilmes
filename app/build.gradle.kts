4fd2d588e422fc766be349e86ae43f832153e18a
Pedro Botelho <114412781+pedropibotelho@users.noreply.github.com>
2024-08-19 16:45:57 -0300

    teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation ("com.github.santalu:maskara:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
