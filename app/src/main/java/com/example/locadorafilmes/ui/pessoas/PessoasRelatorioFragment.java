741ee3c5dbfba8cb4c1a2e65d1b89124f064dd69
pedropibotelho <p2pinheiro2@gmail.com>
2024-08-20 17:45:11 -0300

    criando funções

--- app/src/main/java/com/example/locadorafilmes/ui/pessoas/PessoasRelatorioFragment.java
+++ app/src/main/java/com/example/locadorafilmes/ui/pessoas/PessoasRelatorioFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.locadorafilmes.R;
 
@@ -19,6 +20,42 @@ public class PessoasRelatorioFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_pessoas_relatorio, container, false);
 
+        Button btnProcurar = getActivity().findViewById(R.id.btn_relatorio_pessoas_procurar);
+        btnProcurar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                consultar();
+            }
+        });
+
+        Button btnAlterar = getActivity().findViewById(R.id.btn_relatorio_pessoas_alterar);
+        btnAlterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alterar();
+            }
+        });
+
+        Button btnExcluir = getActivity().findViewById(R.id.btn_relatorio_pessoas_excluir);
+        btnExcluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                excluir();
+            }
+        });
+
         return rootView;
+    }
+
+    public void consultar(){
+
+    }
+
+    public void alterar(){
+
+    }
+
+    public void excluir(){
+
     }
 }
