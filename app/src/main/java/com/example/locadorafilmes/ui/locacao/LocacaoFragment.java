d2c390f7cfc9b0d92736e49bd20fc4e783fbd561
Pedro Botelho <114412781+pedropibotelho@users.noreply.github.com>
2024-09-04 23:12:42 -0300

    Tela locacao concluída

--- app/src/main/java/com/example/locadorafilmes/ui/locacao/LocacaoFragment.java
+++ app/src/main/java/com/example/locadorafilmes/ui/locacao/LocacaoFragment.java
@@ -16,6 +16,8 @@ public class LocacaoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_locacao, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_locacao, container, false);
+
+        return rootView;
     }
 }
